d91dfceda42a3d1a715c6b4c2f4c6191c0db0570
Miguel Martin <miguelmartin97@hotmail.com>
2021-09-18 17:50:35 +0200

    num dinners animation working

--- src/app/pages/create-recipe/basic-info/basic-info.page.ts
+++ src/app/pages/create-recipe/basic-info/basic-info.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { enterAnimation, leaveAnimation } from 'src/app/animations/selector';
 import { SelectorComponent } from 'src/app/components/selector/selector.component';
@@ -7,25 +7,45 @@ import { RecipeType } from 'src/app/enums/recipe-type';
 import { Recipe } from 'src/app/models/recipe';
 import { ModalsService } from 'src/app/services/modals.service';
 import { SessionService } from 'src/app/services/session.service';
-
+import { Flip } from 'number-flip'
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-basic-info',
   templateUrl: './basic-info.page.html',
   styleUrls: ['./basic-info.page.scss'],
 })
-export class BasicInfoPage implements OnInit {
-
+export class BasicInfoPage implements OnInit, AfterViewInit {
+  
   constructor(
     private modalSvc:ModalsService,
     private sessionSvc:SessionService,
-  ) { }
+    private utils:UtilsService
+    ) { }
+
+    flip: any;
+  @ViewChild('dinners', { read: ElementRef }) private dinners: ElementRef;
+
+  async ngAfterViewInit() {
+
+    this.recipe.people = 1;
+    await this.utils.wait(2)
+
+    this.flip = new Flip({
+      node: this.dinners.nativeElement,
+      from: '01',
+      duration: 0.4
+    });
+
+
+  }
 
   recipe:Recipe
 
+
   ngOnInit() {
     this.recipe = this.sessionSvc.recipe;
-    this.recipe.people = 1;
+
   }
 
   async selectRecipeTime(){
@@ -39,7 +59,22 @@ export class BasicInfoPage implements OnInit {
 
   start(){
     console.log(this.sessionSvc.recipe);
-    
+  }
+  
+  addDinner(){
+    if(this.recipe.people>=99) return;
+    this.recipe.people++;
+    this.flip.flipTo({
+      to: this.recipe.people
+    });
+  }
+
+  restDinner(){
+    if(this.recipe.people<=1) return;
+    this.flip.flipTo({
+      to: --this.recipe.people,
+      direct: false
+    });
   }
 
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -3,8 +3,6 @@ import { IonContent, ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { first } from 'rxjs/operators';
 import { Platform } from '@ionic/angular';
-import { Clipboard } from '@capacitor/clipboard';
-import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 @Injectable({
   providedIn: 'root'
@@ -57,20 +55,20 @@ export class UtilsService {
    }
  }
 
- async copy(value:string, msgWord:string = "copied", vibrate:boolean = true){
-    Clipboard.write({
-      string: value
-    });
+//  async copy(value:string, msgWord:string = "copied", vibrate:boolean = true){
+//     Clipboard.write({
+//       string: value
+//     });
 
-    const translatedMsg = await this.trans(msgWord, null, 'copy');
-    const message = `<ion-icon name="clipboard-outline"></ion-icon> ${translatedMsg}`;
+//     const translatedMsg = await this.trans(msgWord, null, 'copy');
+//     const message = `<ion-icon name="clipboard-outline"></ion-icon> ${translatedMsg}`;
 
-    if(vibrate && this.platform.is('capacitor')) Haptics.impact({style: ImpactStyle.Medium});
+//     if(vibrate && this.platform.is('capacitor')) Haptics.impact({style: ImpactStyle.Medium});
 
-    this.toastController.create({
-      message
-      , duration: 2000
-      , cssClass: 'simple'
-    }).then(toast => toast.present());
-  }
+//     this.toastController.create({
+//       message
+//       , duration: 2000
+//       , cssClass: 'simple'
+//     }).then(toast => toast.present());
+//   }
 }
